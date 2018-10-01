cec84bb12a49d5117f3a55ea93156f6522b3310c
Parker Gordon <parker.gordon@tstllc.net>
2018-10-01 08:20:23 -0400

    Added bootstrap styling, first pass request of request to people-api

--- people-app-ui/src/App.js
+++ people-app-ui/src/App.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Table , Grid } from 'react-bootstrap';
+
+
+class People extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:9000/people')
+      .then(results => {
+        return results.json();
+      }).then(data => {
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <Table striped bordered >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Job Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+        </tbody>
+      </Table>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -8,11 +54,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">People Application</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p/>
+        <Grid>
+          <p align="left" className="App-intro">
+            Directory
+          </p>
+          <People />
+          <Button>
+            Character Lookup
+          </Button>
+        </Grid>
       </div>
     );
   }
